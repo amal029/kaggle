6fdd458ae94306e23a3b6ab13d3c99ad6329de27
Avinash Malik <avinash.malik@auckland.ac.nz>
2019-07-14 15:41:10 +1200

    Added sentiment analysis for modi.py
    Signed-off-by: Avinash Malik <avinash.malik@auckland.ac.nz>

--- modi.py
+++ modi.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import string
 import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 
 
@@ -13,8 +14,23 @@ def get_newline_index(s):
             j = i
             break
     return ''.join([i for i in s[j:]
-                    if i in string.printable and
-                    (i not in string.punctuation)])
+                    if i in string.printable])
+
+
+def sentiment_analysis(speech):
+    # Do sentiment analysis for Modi's speeches over time
+    sid = SentimentIntensityAnalyzer()
+    sentences = speech.split('.')
+    pos, neg, neu = 0, 0, 0
+    for s in sentences:
+        ss = sid.polarity_scores(s)
+        if ss['pos'] >= 0.5:
+            pos += 1
+        elif ss['neg'] >= 0.5:
+            neg += 1
+        elif ss['neu'] >= 0.5:
+            neu += 1
+    return (pos, neg, neu)
 
 
 def main(fileName):
@@ -25,6 +41,7 @@ def main(fileName):
     # Tokenize words
     speeches = df['Speech_text']  # Series of speeches
     speeches_time = []
+    sentiment_time = []
     for speech in speeches:
         speech_tokens = nltk.word_tokenize(speech)
         # Now remove the stopwords
@@ -38,6 +55,10 @@ def main(fileName):
         # put words in order across all speeches
         # print(speech_dist.most_common(5))
         speeches_time.append(speech_dist.most_common(10))
+        # Add the sentiment scores to the list
+        sentiment_time.append(sentiment_analysis(speech))
+    df['pos_sentimement'], df['neg_sentimement'], df['neu_sentimement'] \
+        = zip(*sentiment_time)
     # Flattened
     speeches_time = [j for i in speeches_time
                      for j in i]
@@ -51,7 +72,9 @@ def main(fileName):
     most_talked = (sorted(list(final_dict.items()), key=lambda v: v[1],
                           reverse=True))[:10]
     # Modi talks most about these 10 things over time
-    print(most_talked)
+    print('most talked about nouns: ', most_talked)
+    # Write out the csv
+    df.to_csv('/tmp/modi_result.csv')
 
 
 if __name__ == '__main__':
